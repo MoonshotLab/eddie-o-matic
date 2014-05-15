4a9dd64debd6691f29a5d0962b2ef25d84ba6a21
Joe Longstreet <joelongstreet@gmail.com>
2014-05-15 09:16:10 -0500

    move floor finder into seperate method, more logging improvements

--- libs/mail.js
+++ libs/mail.js
@@ -7,6 +7,51 @@ var contextClient = new ContextIO.Client({
 });
 
 
+var findFloor = function(content){
+  var floor = 1;
+
+  // Try a good guess, look for the word floor and any numbers
+  // which are close to the word
+  if(content.indexOf('floor') != -1){
+    var index = content.indexOf('floor');
+    var sub = content.substring(index-10, index+10);
+    if(sub.indexOf('1') != -1) floor = 1;
+    else if(sub.indexOf('2') != -1) floor = 2;
+    else if(sub.indexOf('3') != -1) floor = 3;
+    else if(sub.indexOf('4') != -1) floor = 4;
+  }
+
+  // Give up and look at entire string, searching for
+  // numbers and their appropriate suffix
+  if(!floor){
+    if(content.indexOf('1st') != -1) floor = 1;
+    else if(content.indexOf('2nd') != -1) floor = 2;
+    else if(content.indexOf('3rd') != -1) floor = 3;
+    else if(content.indexOf('4th') != -1) floor = 4;
+  }
+
+  // More guessing
+  if(!floor){
+    if(content.indexOf('first') != -1) floor = 1;
+    else if(content.indexOf('second') != -1) floor = 2;
+    else if(content.indexOf('third') != -1) floor = 3;
+    else if(content.indexOf('fourth') != -1) floor = 4;
+  }
+
+  // Look at the first 20 characters... trying to
+  // avoid the e-mail signature
+  if(!floor){
+    var subst = content.substring(0, 20);
+    if(subst.indexOf('1') != -1) floor = 1;
+    else if(subst.indexOf('2') != -1) floor = 2;
+    else if(subst.indexOf('3') != -1) floor = 3;
+    else if(subst.indexOf('4') != -1) floor = 4;
+  }
+
+  return floor;
+};
+
+
 exports.fetchMessage = function(message){
   var deferred = Q.defer();
 
@@ -34,7 +79,7 @@ exports.parseMessage = function(message){
 
   if(content.length < 700){
     var matches = [];
-    var floor = 1;
+    var floor = findFloor(content);
 
     // Find matching terms
     config.terms.some(function(term){
@@ -45,44 +90,6 @@ exports.parseMessage = function(message){
         }
       });
     });
-
-    // Try a good guess, look for the word floor and any numbers
-    // which are close to the word
-    if(content.indexOf('floor') != -1){
-      var index = content.indexOf('floor');
-      var sub = content.substring(index-10, index+10);
-      if(sub.indexOf('1') != -1) floor = 1;
-      else if(sub.indexOf('2') != -1) floor = 2;
-      else if(sub.indexOf('3') != -1) floor = 3;
-      else if(sub.indexOf('4') != -1) floor = 4;
-    }
-
-    // Give up and look at entire string, searching for
-    // numbers and their appropriate suffix
-    if(!floor){
-      if(content.indexOf('1st') != -1) floor = 1;
-      else if(content.indexOf('2nd') != -1) floor = 2;
-      else if(content.indexOf('3rd') != -1) floor = 3;
-      else if(content.indexOf('4th') != -1) floor = 4;
-    }
-
-    // More guessing
-    if(!floor){
-      if(content.indexOf('first') != -1) floor = 1;
-      else if(content.indexOf('second') != -1) floor = 2;
-      else if(content.indexOf('third') != -1) floor = 3;
-      else if(content.indexOf('fourth') != -1) floor = 4;
-    }
-
-    // Look at the first 20 characters... trying to
-    // avoid the e-mail signature
-    if(!floor){
-      var sub = content.substring(0, 20);
-      if(sub.indexOf('1') != -1) floor = 1;
-      else if(sub.indexOf('2') != -1) floor = 2;
-      else if(sub.indexOf('3') != -1) floor = 3;
-      else if(sub.indexOf('4') != -1) floor = 4;
-    }
 
     if(matches.length){
       var matchedNames = [];
@@ -98,6 +105,8 @@ exports.parseMessage = function(message){
         '\n',
         'subject:',
         message.subject,
+        'floor:',
+        floor,
         '\n --------------'
       );
 
