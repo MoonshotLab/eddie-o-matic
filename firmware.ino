787cf65fdbb11f1ce984a03d4ff7a37da5eaa194
Eugene Williams <ewilliam@be6744.bedomain.beap.com>
2014-06-17 16:04:39 -0500

    added a button to test it also now rings to tell the floor it is on

--- firmware.ino
+++ firmware.ino
@@ -1,52 +1,94 @@
 Servo FLOOR_SELECTOR;
 Servo BELL;
-int pins[] = {D0, D1, D2, D3, D4, D5};
+
+int lights[] = {D0, D1, D2, D3, D4, D5};
+int locationMap[] = {157, 115, 65, 20};
+int button = D6;
 
 
-// Pass in params like 1,100
+// Pass in params like 1,3 pin for food then floor
 int updateState(String param)
 {
-  int pinNumber = param[0] - '0';
-  int location = param.substring(2, param.length()).toInt();
+    int pinNumber = param[0] - '0';
+    int location = param[2] - '0';
+    foodAndFloor(lights[pinNumber], locationMap[location-1]);
+    ringBellTimes(location);
+    delay(15000);
+    reset();
+    return 1;
+}
 
-  BELL.write(165);
-  digitalWrite(pins[pinNumber], 1);
-  delay(100);
-  BELL.write(125);
-  delay(300);
+void foodAndFloor(int foodPin, int degrees)
+{
+    digitalWrite(foodPin, 1);
+    FLOOR_SELECTOR.write(degrees);
+}
 
-  FLOOR_SELECTOR.write(location);
-  delay(15000);
+void ringBellTimes(int occ)
+{
+    for(int i=0; i<occ; i++)
+    {
+        BELL.write(165);
+        delay(500);
+        BELL.write(125);
+        delay(500);
+    }
+}
 
-  // Reset
-  for(int i=0; i<6; i++){
-    digitalWrite(pins[i], 0);
-  }
-  FLOOR_SELECTOR.write(88);
-
-  return 1;
+void reset()
+{
+    for(int i=0; i<6; i++)
+    {
+        digitalWrite(lights[i], 0);
+    }
+    FLOOR_SELECTOR.write(88);
 }
 
 
 void setup()
 {
-  FLOOR_SELECTOR.attach(A0);
-  BELL.attach(A4);
+    FLOOR_SELECTOR.attach(A0);
+    BELL.attach(A4);
+  
+    pinMode(button,INPUT);
 
-  FLOOR_SELECTOR.write(88);
-  delay(500);
-  BELL.write(125);
+    FLOOR_SELECTOR.write(88);
+    delay(500);
+    BELL.write(125);
 
-  for(int i=0; i<6; i++){
-    pinMode(pins[i], OUTPUT);
-    digitalWrite(pins[i], 1);
-  }
+    for(int i=0; i<6; i++)
+    {
+        pinMode(lights[i], OUTPUT);
+        digitalWrite(lights[i], 1);
+    }
 
-  delay(3000);
+    delay(3000);
 
-  for(int i=0; i<6; i++){
-    digitalWrite(pins[i], 0);
-  }
+    for(int i=0; i<6; i++){
+        digitalWrite(lights[i], 0);
+    }
 
-  Spark.function("updateState", updateState);
+    Spark.function("updateState", updateState);
+}
+
+void loop()
+{
+    if(digitalRead(button)==1)
+    {
+        foodAndFloor(D1, 115);
+        
+        BELL.write(165);
+        delay(150);
+        BELL.write(125);
+        delay(150);
+        
+        BELL.write(165);
+        delay(300);
+        BELL.write(125);
+        delay(150);
+        
+        delay(3000);
+        
+        reset();
+    } 
 }
