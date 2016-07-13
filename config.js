ec32886e75f53f2372fb4aab1ddea755849c62bc
Joe Longstreet <joelongstreet@gmail.com>
2016-07-13 15:04:48 -0500

    new pin mapping

--- config.js
+++ config.js
@@ -3,37 +3,37 @@ exports.bannedTerms = ['foodsaver'];
 exports.categories = [
   {
     // Sweets
-    pinId: 0,
+    pinId: 1,
     labels: ['cake', 'ice cream', 'desert', 'dessert', 'cookie', 'chocolate'],
     name: 'Sweets'
   },
   {
     // Drinks
-    pinId: 1,
+    pinId: 0,
     labels: ['margarita', 'beer', 'alcohol', 'thirsty thursday'],
     name: 'Drinks'
   },
   {
     // Left Over Lunch
-    pinId: 2,
+    pinId: 3,
     labels: ['left over', 'leftover', 'buffet', 'food', 'sandwich', 'salad', 'dean', 'deluca', 'muffaletta', 'meat', 'pizza'],
     name: 'Left Over Lunch'
   },
   {
     // Pastries
-    pinId: 3,
+    pinId: 2,
     labels: ['cup cake', 'cupcake', 'donut', 'bagel', 'muffin'],
     name: 'Pastries'
   },
   {
     // Coffee Truck
-    pinId: 4,
+    pinId: 5,
     labels: ['coffee'],
     name: 'Coffee Truck'
   },
   {
     // Food Truck
-    pinId: 5,
+    pinId: 4,
     labels: ['food truck', 'truck'],
     name: 'Food Truck'
   }
