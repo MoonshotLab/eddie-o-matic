98bce3a9d2f570660872a02b278e532ae18d2a8a
Preston Richey <prichey@barkleyus.com>
2016-10-10 16:26:43 -0500

    Use request rather than needle

--- libs/duino.js
+++ libs/duino.js
@@ -1,6 +1,7 @@
 var env = require('../env-config.js')();
 var Q = require('q');
-var needle = require('needle');
+// var needle = require('needle');
+var request = require('request');
 var config = require('../config');
 
 exports.broadcast = function(opts){
@@ -12,21 +13,42 @@ exports.broadcast = function(opts){
     '/updateState'
   ].join('');
 
-  var postParams = [
-    'access_token=',
-    env.SPARK_CORE_TOKEN,
-    '&params=',
-    opts.matchedCategories[0].pinId,
-    ',',
-    opts.floor
-  ].join('');
+  // var postParams = [
+  //   'access_token=',
+  //   env.SPARK_CORE_TOKEN,
+  //   '&params=',
+  //   opts.matchedCategories[0].pinId,
+  //   ',',
+  //   opts.floor
+  // ].join('');
+  //
+  // console.log('url:', url);
+  // console.log('postParams', postParams);
+  //
+  // needle.post(url, postParams, function(err, res){
+  //   console.log('attemtping to post to server');
+  //   if(err) {
+  //     console.log('error in posting to server');
+  //     deferred.reject(err);
+  //   }
+  //   else console.log('sent to server');
+  //
+  //   deferred.resolve({
+  //     err: err,
+  //     res: res
+  //   });
+  // });
 
-  console.log('url:', url);
-  console.log('postParams', postParams);
-
-  needle.post(url, postParams, function(err, res){
-    console.log('attemtping to post to server');
-    if(err) {
+  request.post({
+    url: url,
+    qs: {
+      access_token: env.SPARK_CORE_TOKEN,
+      params: opts.matchedCategories[0].pinId + ',' + opts.floor
+    },
+    method: 'POST'
+  }, function(err, res) {
+    console.log('attempting to post to server');
+    if (err) {
       console.log('error in posting to server');
       deferred.reject(err);
     }
